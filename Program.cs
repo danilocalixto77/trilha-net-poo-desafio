aa0a930af2f3d68b937521a5c67d6692e23de2e5
danilocalixto77 <danilocalixto@gmail.com>
2023-11-04 23:23:30 -0400

    feet: Conclusão do desafio C# POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testando Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Nokia A2000", imei: "111111111", memoria : 8);
+
+nokia.Ligar();
+Console.WriteLine($"{nokia.Numero} ");
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Testando Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero: "12349876", modelo: "8S", imei: "987987987", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
